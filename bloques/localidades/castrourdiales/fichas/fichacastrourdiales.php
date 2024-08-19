89ef88754b706e35cc815c511076114fe6093846
luxen4 <superlaya51@gmail.com>
2024-08-19 17:20:11 +0200

    Simplificación de cambio de imagenes en Castro Urdiales1

--- bloques/localidades/castrourdiales/fichas/fichacastrourdiales.php
+++ bloques/localidades/castrourdiales/fichas/fichacastrourdiales.php
@@ -139,6 +139,8 @@
         <?php require './../../../../bloques/estructura/footer.php'; ?>
     </footer>
 
+    <?php require './../../../../bloques/estructura/anuncios/js/logica_anuncios.php'; ?>
+
     <!-- Enlace a los scripts de Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
@@ -184,70 +186,3 @@ Para una imagen que muestre el puerto deportivo cercano a la playa.*/
 
 
 ?>
-
-
-
-<?php
-// Definir categorías y grupos
-$categories = [
-    "sandals_man" => 2,
-    "shirts_modelo_derecha" => 2,
-    "shirts_izquierda" => 2,
-    "sneakers_izquierda_15" => 2,
-    "sandals_woman_izda_15" => 2
-];
-
-// Función para generar las constantes de imagen en JavaScript
-function generateImageConstants($categories, $total_images = 4) {
-    foreach ($categories as $category => $group) {
-        for ($i = 1; $i <= $total_images; $i++) {
-            $variable_name = "{$category}_{$group}_{$i}";
-            if (isset($GLOBALS["images_" . $variable_name])) {
-                echo "const images_{$variable_name} = " . json_encode($GLOBALS["images_" . $variable_name]) . ";\n";
-            } else {
-                echo "const images_{$variable_name} = [];\n";
-            }
-        }
-    }
-}
-
-// Definir las bases y rangos de grupos para la inicialización de los índices
-function addProperties($categories, $total_images = 4) {
-    foreach ($categories as $category => $group) {
-        for ($i = 1; $i <= $total_images; $i++) {
-            echo "currentIndex['{$category}_{$group}_{$i}'] = 0;\n";
-        }
-    }
-}
-?>
-
-<script>
-    let currentIndex = {};
-
-    <?php generateImageConstants($categories); ?>
-    <?php addProperties($categories); ?>
-
-    function updateImage(image_link, image, cte, indexKey) {
-        currentIndex[indexKey] = (currentIndex[indexKey] + 1) % cte.length;
-        document.getElementById(image_link).href = cte[currentIndex[indexKey]].href;
-        document.getElementById(image).src = cte[currentIndex[indexKey]].src;
-        document.getElementById(image).alt = cte[currentIndex[indexKey]].alt;
-    }
-
-    function changeImages() {
-        <?php
-        foreach ($categories as $articulo => $group) {
-            $images = [];
-            for ($i = 1; $i <= 4; $i++) {
-                $images[] = "images_{$articulo}_{$group}_{$i}";
-            }
-            foreach ($images as $index => $image) {
-                $i = $index + 1;
-                echo "updateImage('image-link_{$articulo}_{$group}_{$i}', 'image_{$articulo}_{$group}_{$i}', {$image}, '{$articulo}_{$group}_{$i}');\n";
-            }
-        }
-        ?>
-    }
-
-    setInterval(changeImages, 4000);
-</script>

e17def3c058e58ead6dc5a269181c85049370785
Luxen-4 <superlaya51@gmail.com>
2025-06-30 17:46:01 +0200

    2025/05/30/17:45/

--- localidades/cantabria/castro-urdiales/lugares-interes/body/post-header-generico.php
+++ localidades/cantabria/castro-urdiales/lugares-interes/body/post-header-generico.php
@@ -1,20 +1,20 @@
-    <div class="container-xxl py-4">
-      <div class="row">
+<div class="container-xxl py-4">
+  <div class="row">
 
-        <div class="col-12 col-md-9">
-          <?php require PATH_RAIZ_LOCALIDAD_LUGARES_INTERES . "/{$lugar_interes}/body/main.php"; ?>
-        </div>
-
-        <div class="col-12 col-md-3 mt-4 mt-md-0">
-          <div class="mb-4 hidden md:block">
-            <?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_PAGINAS_PLAYA_BODY_MAIN . '/sidebar-playa.php'; ?>
-          </div>
-
-          <?php /*
-          <div class="bg-white border border-gray-300 rounded-lg shadow-sm p-3">
-            <?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_ANUNCIOS_JS . "/anuncios-add-cash.php"; ?>
-          </div> */ ?>
-        </div>
-
-      </div>
+    <div class="col-xl-9 col-lg-8 col-md-8 col-sm-12 col-12">
+      <?php require PATH_RAIZ_LOCALIDAD_LUGARES_INTERES . "/{$lugar_interes}/body/main.php"; ?>
     </div>
+
+    <div class="col-xl-3 col-lg-4 col-md-4 d-none d-md-block sidebar-anuncios">
+      <div class="mb-4">
+        <?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_PAGINAS_PLAYA_BODY_MAIN . '/sidebar-playa.php'; ?>
+      </div>
+
+      <?php /*
+      <div class="bg-white border border-gray-300 rounded-lg shadow-sm p-3">
+        <?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_ANUNCIOS_JS . "/anuncios-add-cash.php"; ?>
+      </div> */ ?>
+    </div>
+
+  </div>
+</div>

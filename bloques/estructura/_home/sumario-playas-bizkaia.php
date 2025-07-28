5710ad3ebb44e8d83d006ff768048b49b27f84e3
luxen4 <superlaya51@gmail.com>
2025-07-28 22:09:32 +0200

    28/07/2025-00:00h-home

--- bloques/estructura/_home/sumario-playas-bizkaia.php
+++ bloques/estructura/_home/sumario-playas-bizkaia.php
@@ -23,6 +23,8 @@ function render_playa_card($playa_name, $presentacion, $region_minuscula, $local
 
 
 <?php $localidad = 'Bakio'; $localidad_minuscula = strtolower($localidad);?>
+
+
 <h3 id="<?= $localidad_minuscula; ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
 
 <?php
@@ -47,90 +49,45 @@ $presentacion = '🌿 La Playa de Astondoko, es una playa natural situada junto 
 
 
 
-<?php
-$carpeta = 'playa-de-laga';
-$playa_name = 'Playa de Laga';
-$localidad = 'Ibarrangelu';
-$localidad_minuscula = strtolower($localidad);  // "ibarrangelu"
-$region = 'Bizkaia';
-$region_minuscula = strtolower($region);        // "bizkaia"
-$archivo_name = 'laga-ibarrangelu';
+<?php $localidad = 'Ibarrangelu'; ?>
+<h3 id="<?= strtolower($localidad); ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
 
+<?php
+$playa_name = 'Playa de Laga'; $carpeta = strtolower(str_replace(' ', '-', $playa_name)); $archivo_name = 'laga-ibarrangelu';
 $presentacion = 'La Playa de Laya, ubicada en Ibarrangelu, Bizkaia, es una playa virgen y tranquila dentro de la Reserva de la Biosfera de Urdaibai. Destaca por su entorno natural protegido, aguas limpias y olas apreciadas por surfistas. Con acceso sencillo y sin masificaciones, Laya es perfecta para disfrutar de la costa vasca en un entorno salvaje y relajante.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
 ?>
-
-<?php $pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS = "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}"; ?>
-
-<?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME. '/body/card-playa.php'; ?>
+<?php render_playa_card($playa_name, $presentacion, $region_minuscula, $localidad_minuscula, $targeta, $archivo_name);?>
 
 
-
+<?php $localidad = 'Muskiz'; ?>
+<h3 id="<?= strtolower($localidad); ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
 
 <?php
-$carpeta = 'playa-de-la-arena';
-$playa_name = 'Playa de La Arena';
-$localidad = 'Muskiz';
-$localidad_minuscula = strtolower($localidad);  // "muskiz"
-$region = 'bizkaia';
-$region_minuscula = strtolower(str_replace(' ', '-', $region));  // "bizkaia"
-$archivo_name = 'la-arena-muskiz';
-
+$playa_name = 'Playa de La Arena'; $carpeta = strtolower(str_replace(' ', '-', $playa_name)); $archivo_name = 'la-arena-muskiz';
 $presentacion = 'La Playa de La Arena, situada entre Muskiz (bizkaia) y el límite con Cantabria, es una extensa playa de arena fina con fuerte tradición surfera. Ideal para familias, cuenta con todos los servicios necesarios, accesos cómodos y está rodeada de rutas costeras y espacios naturales.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
 ?>
-<?php $pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS = "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}"; ?>
+<?php render_playa_card($playa_name, $presentacion, $region_minuscula, $localidad_minuscula, $targeta, $archivo_name);?>
 
-<?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME . '/body/card-playa.php'; ?>
 
+<?php $localidad = 'Sopelana';?>
+<h3 id="<?= strtolower($localidad); ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
 
 <?php
-$carpeta = 'playa-de-arrietara';
-$playa_name = 'Playa de Arrietara';
-$localidad = 'Sopelana';
-$localidad_minuscula = strtolower($localidad);  // "sopelana"
-$region = 'bizkaia';
-$region_minuscula = strtolower(str_replace(' ', '-', $region));  // "bizkaia"
-$archivo_name = 'arrietara-sopelana';
-
+$playa_name = 'Playa de Arrietara'; $carpeta = strtolower(str_replace(' ', '-', $playa_name)); $archivo_name = 'arrietara-sopelana';
 $presentacion = 'La Playa de Arrietara, situada en Sopelana (Bizkaia), es una playa popular entre surfistas y familias, con arena fina, buen acceso y servicios. Está rodeada de espacios naturales ideales para pasear y disfrutar de la costa vasca.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
 ?>
-<?php $pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS = "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}"; ?>
-
-<?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME . '/body/card-playa.php'; ?>
+<?php render_playa_card($playa_name, $presentacion, $region_minuscula, $localidad_minuscula, $targeta, $archivo_name);?>
 
 
+<?php $localidad = 'Plentzia';?>
+<h3 id="<?= strtolower($localidad); ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
 
 
 <?php
-$carpeta = 'playa-de-plentzia';
-$playa_name = 'Playa de Plentzia';
-$localidad = 'Plentzia';
-$localidad_minuscula = strtolower($localidad);  // "plentzia"
-$region = 'bizkaia';
-$region_minuscula = strtolower(str_replace(' ', '-', $region));  // "bizkaia"
-$archivo_name = 'plentzia-plentzia';
-
+$playa_name = 'Playa de Plentzia'; $carpeta = strtolower(str_replace(' ', '-', $playa_name)); $archivo_name = 'plentzia-plentzia';
 $presentacion = 'La Playa de Plentzia, situada en bizkaia, es una extensa y tranquila playa de arena fina, perfecta para familias y paseos. Cuenta con servicios completos, fácil acceso y está protegida del oleaje fuerte, ideal para nadar y disfrutar del paisaje natural de la costa vasca.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
 ?>
-
-
-
-
-
-<?php $pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS = "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}"; ?>
-
-<?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME . '/body/card-playa.php'; ?>
-
-
-
-
+<?php render_playa_card($playa_name, $presentacion, $region_minuscula, $localidad_minuscula, $targeta, $archivo_name);?>
 
 
 
--- bloques/estructura/_home/sumario-playas-nouvelle-aquitaine.php
+++ bloques/estructura/_home/sumario-playas-nouvelle-aquitaine.php
@@ -1,89 +1,46 @@
 <?php $targeta = PATH_RAIZ_LOCALIDADES . '/index.php'; ?>
-<h2 id="nouvelle-aquitaine" class="text-2xl md:text-3xl font-bold text-center text-<?= $color_page; ?>-800 mb-6">
-  Nouvelle Aquitaine
-</h2>
-<?php
-$carpeta = 'playa-de-la-grande-plage-saint-jean-de-luz';
-$playa_name = 'Grande Plage';
-$localidad = 'Saint Jean de Luz';
-$localidad_minuscula = strtolower(str_replace(' ', '-', $localidad));  // "saint-jean-de-luz"
-$region = 'Nouvelle Aquitaine';
-$region_minuscula = strtolower(str_replace(' ', '-', $region));  // "nouvelle-aquitaine"
-$archivo_name = 'grande-plage-saint-jean-de-luz';
+<?php $publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php'; ?>
 
+<?php $region = 'Nouvelle Aquitaine'; $region_minuscula = strtolower($region);?>
+<h2 id="cantabria" class="text-2xl md:text-3xl font-bold text-center text-<?= $color_page; ?>-800 mb-6"><?= $region; ?></h2>
+
+
+<?php $localidad = 'Saint Jean de Luz'; ?>
+<h3 id="<?= strtolower($localidad); ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
+
+<?php
+$playa_name = 'Grande Plage'; $carpeta = 'playa-de-la-grande-plage-saint-jean-de-luz'; $archivo_name = 'grande-plage-saint-jean-de-luz';
 $presentacion = '🏖️ La Grande Plage de Saint-Jean-de-Luz, situada en el corazón del casco histórico, ofrece aguas tranquilas gracias a su bahía protegida. Es ideal para familias, baños relajantes y paseos junto al paseo marítimo en un entorno con encanto vasco-francés.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
 ?>
+<?php render_playa_card($playa_name, $presentacion, $region_minuscula, $localidad_minuscula, $targeta, $archivo_name);?>
 
 
-<?php 
-$pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS =
-       "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}";
-?>
 
-<?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME . '/body/card-playa.php'; ?>
+
+<?php $localidad = 'Biarritz'; ?>
+<h3 id="<?= strtolower($localidad); ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
+
+
 
 <?php
-/*
-$carpeta = 'playa-de-a-ladeira';  // nombre slug para la playa
-$playa_name = 'Playa de A Ladeira';
-$localidad = 'Baiona';
-$localidad_minuscula = strtolower(str_replace(' ', '-', $localidad));  // "baiona"
-$region = 'Nouvelle-Aquitaine';  // nombre oficial en español o francés según prefieras
-$region_minuscula = strtolower(str_replace(' ', '-', $region));       // "nouvelle-aquitaine"
-$archivo_name = 'baiona-nouvelle-aquitaine';
-
-$presentacion = '🌊 La Playa de A Ladeira en Baiona es un lugar tranquilo y familiar, perfecto para disfrutar de la naturaleza, baños y paseos en la hermosa costa vasco-francesa.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
-
-$pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS =
-       "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}";
-
-require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME . '/body/card-playa.php';*/
-?>
-
-<?php
-$carpeta = 'playa-de-la-grande-plage-biarritz';
-$playa_name = 'La Grande Plage';
-$localidad = 'Biarritz';
-$localidad_minuscula = strtolower(str_replace(' ', '-', $localidad));  // "biarritz"
-$region = 'Nouvelle-Aquitaine';  // nombre oficial en español o francés según prefieras
-$region_minuscula = strtolower(str_replace(' ', '-', $region));       // "nouvelle-aquitaine"
-$archivo_name = 'grande-plage-biarritz';
-
+$playa_name = 'La Grande Plage'; $carpeta = 'playa-de-la-grande-plage-biarritz'; $archivo_name = 'grande-plage-biarritz';
 $presentacion = '🌊 La Grande Plage en Biarritz es la playa más emblemática de la ciudad, famosa por sus olas para surfear, su amplio paseo marítimo y su animado ambiente durante todo el año.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
-
-$pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS =
-       "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}";
-
-require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME . '/body/card-playa.php';
 ?>
+<?php render_playa_card($playa_name, $presentacion, $region_minuscula, $localidad_minuscula, $targeta, $archivo_name);?>
 
 
 
+
+<?php $localidad = 'Capbreton'; ?>
+<h3 id="<?= strtolower($localidad); ?>" class="text-2xl md:text-2xl font-bold text-left text-<?= $color_page; ?>-800 mb-6 underline"><?= $localidad; ?></h3>
 
 <?php
-$carpeta = 'playa-de-la-plage-centrale-capbreton';  // slug para la playa
-$playa_name = 'Plage Centrale';
-$localidad = 'Capbreton';
-$localidad_minuscula = strtolower(str_replace(' ', '-', $localidad));  // "capbreton"
-$region = 'Nouvelle-Aquitaine';
-$region_minuscula = strtolower(str_replace(' ', '-', $region));       // "nouvelle-aquitaine"
-$archivo_name = 'playa-de-la-plage-centrale-capbreton';
-
+$playa_name = 'Plage Centrale'; $carpeta = 'playa-de-la-plage-centrale-capbreton'; $archivo_name = 'playa-de-la-plage-centrale-capbreton';
 $presentacion = '🏄 La Plage Centrale en Capbreton es una playa urbana muy popular, ideal para surfistas y familias que disfrutan del mar y el ambiente costero en Landes.';
-
-$publicidad = './bloques/estructura/sections_generico/Carruseles_CSV/prueba_sneakers.php';
-
-$pATH_HREF_RAIZ_LOCALIDADES_REGION_LOCALIDAD_LUGARES_INTERES_PLAYAS =
-       "/Blog_Playas2025/localidades/{$region_minuscula}/{$localidad_minuscula}/lugares-interes/playas/{$carpeta}";
-
-require PATH_RAIZ_BLOQUES_ESTRUCTURA_HOME . '/body/card-playa.php';
 ?>
+<?php render_playa_card($playa_name, $presentacion, $region_minuscula, $localidad_minuscula, $targeta, $archivo_name);?>
+
+
 
 
 

2d9a77acc8fa1050306ced861c4057a6a986b0c7
luxen4 <superlaya51@gmail.com>
2025-08-15 00:01:22 +0200

    15/08/2025-Lugares Interés Nouvelle-Aquitaine2

--- localidades/carga-variables-generico-lugares-interes.php
+++ localidades/carga-variables-generico-lugares-interes.php
@@ -1,4 +1,4 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . "/Blog_Playas2025/bloques/estructura/variables_comunes/defines.php"; ?>
 <?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_PAGINAS_PLAYA_HEAD . '/graph_Meta_Tags_property-generico.php'; ?>
 <?php require PATH_RAIZ_BLOQUES_ESTRUCTURA_PAGINAS_PLAYA_HEAD . '/lugares-interes/variables-especiales.php'; ?>
-<?php require PATH_RAIZ_LOCALIDADES_CARPETA_HEAD_VARIABLES . "/data-generico.php";  ?><?php echo( PATH_RAIZ_LOCALIDADES_CARPETA_HEAD_VARIABLES . "/data-generico.php"); ?>
+<?php require PATH_RAIZ_LOCALIDADES_CARPETA_HEAD_VARIABLES . "/data-generico.php";  ?>
